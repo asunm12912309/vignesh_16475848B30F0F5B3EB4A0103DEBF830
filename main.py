b2b5d2e86b2d3ed4ef254bac547795bcb885ddc3
asunm12912309 <144411021+asunm12912309@users.noreply.github.com>
2023-09-26 10:07:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Please enter a positive value.")
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
-
-    def display_balance(self):
-        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+  return indices
 
 
-# Creating an instance of the BankAccount class
-my_account = BankAccount("123456789", "vignesh", 1000)
-
-# Testing deposit and withdrawal functionality
-my_account.display_balance()
-my_account.deposit(500)
-my_account.withdraw(200)
-my_account.display_balance()
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
